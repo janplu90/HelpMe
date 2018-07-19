614a9653e0d650d865f43b795adfed5e0cc2f1ad
janplu90 <janplu90@gmail.com>
2018-07-19 17:55:51 +0200

    first commit

---
+++ HelpMe.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,48 @@
+﻿using HelpMe.Domain.Abstract;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Moq;
+using HelpMe.Domain.Entities;
+
+namespace HelpMe.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product>
+            {
+                new Product { Name = "Piłka nożna", Price = 25},
+                new Product { Name = "Deska surfingowa", Price = 179},
+                new Product { Name = "Buty do biegania", Price = 95}
+
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+
+        }
+    }
+}
